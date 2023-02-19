1881f98b57f12edb8b77bc79d2c1eecf665d7b05
Aazad Dahal <aazad.dahal@gurzu.net>
2023-02-19 21:05:40 +0545

    feat: Setup for redux-persist

--- index.js
+++ index.js
@@ -3,16 +3,19 @@
  */
 
 import React from 'react';
-import {store} from '@app/redux/store';
+import {persistor, store} from '@app/redux/store';
 import {AppRegistry} from 'react-native';
 import {Provider} from 'react-redux';
 import App from './App';
 import {name as appName} from './app.json';
+import {PersistGate} from 'redux-persist/integration/react';
 
 const Root = () => {
   return (
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   );
 };
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,24 @@
-import {configureStore} from '@reduxjs/toolkit';
-import {combineReducers} from 'redux';
+import {configureStore, combineReducers} from '@reduxjs/toolkit';
+import {persistReducer, persistStore} from 'redux-persist';
 import {authReducer, fitnessReducer} from './slices';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+};
 
 const rootReducer = combineReducers({
-  auth: authReducer,
+  auth: persistReducer(persistConfig, authReducer),
   fitness: fitnessReducer,
 });
 
 export const store = configureStore({
   reducer: rootReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
+
+export const persistor = persistStore(store);
