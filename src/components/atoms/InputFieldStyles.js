9e3b8f555b48afa023d7cda2e4c072e28899b9f3
Aazad Dahal <aazad.dahal@gurzu.net>
2023-02-21 15:40:09 +0545

    feat: Create external stylesheet for <InputField/>

---
+++ src/components/atoms/InputFieldStyles.js
@@ -0,0 +1,26 @@
+import {StyleSheet} from 'react-native';
+import {Colors} from '@app/constants';
+import {heightToDp, widthToDp} from '@app/utils';
+
+export const Styles = StyleSheet.create({
+  inputMainContainer: {
+    // flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    borderRadius: widthToDp(40),
+    backgroundColor: Colors.inputBackgroundWhite,
+    paddingVertical: heightToDp(3),
+  },
+  iconContainer: {
+    // flex: 1,
+    paddingLeft: widthToDp(18),
+    paddingRight: widthToDp(5),
+  },
+  textInputStyle: {
+    flex: 1,
+    paddingRight: widthToDp(15),
+    fontFamily: 'Poppins',
+    fontSize: widthToDp(14),
+  },
+});
