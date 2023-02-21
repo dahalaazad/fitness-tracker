b07d99e7bbd413e326dd470cd1b267757237e175
Aazad Dahal <aazad.dahal@gurzu.net>
2023-02-21 15:38:47 +0545

    feat: Add svg icons for input field

--- src/assets/svg/index.js
+++ src/assets/svg/index.js
@@ -1,5 +1,7 @@
 import GoogleLogo from './google-logo.svg';
 import FbLogo from './fb-logo.svg';
 import AppleLogo from './apple-logo.svg';
+import EmailIcon from './email-icon.svg';
+import PWIcon from './pw-icon.svg';
 
-export {GoogleLogo, FbLogo, AppleLogo};
+export {GoogleLogo, FbLogo, AppleLogo, EmailIcon, PWIcon};
