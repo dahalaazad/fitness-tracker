acbfe011d2f4f9f47e4fdd9bb3bdbb5a1684a4b0
Aazad Dahal <aazad.dahal@gurzu.net>
2023-02-27 21:25:14 +0545

    feat: Create slice for authentication

--- src/redux/slices/auth/authSlice.js
+++ src/redux/slices/auth/authSlice.js
@@ -1,11 +1,49 @@
-import {createSlice} from '@reduxjs/toolkit';
+import {createAsyncThunk, createSlice} from '@reduxjs/toolkit';
+import axios from 'axios';
+import Config from 'react-native-config';
 
-const initialState = {};
+const initialState = {
+  isLoggedIn: false,
+  loading: false,
+  userToken: null,
+  error: null,
+  userInfo: {},
+};
 
+export const signupWithEmail = createAsyncThunk(
+  'auth/signupWithEmail',
+  async (userSignUpInfo, {rejectWithValue}) => {
+    try {
+      const signUpResponse = await axios.post(
+        `${Config.API_URL}/signup`,
+        userSignUpInfo,
+      );
+      return signUpResponse;
+    } catch (error) {
+      return rejectWithValue(error);
+    }
+  },
+);
 export const authSlice = createSlice({
   name: 'auth',
   initialState,
   reducers: {},
+  extraReducers: builder => {
+    builder
+      .addCase(signupWithEmail.pending, state => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(signupWithEmail.fulfilled, (state, {payload}) => {
+        state.loading = false;
+        state.isLoggedIn = true;
+        state.userInfo = payload?.data;
+      })
+      .addCase(signupWithEmail.rejected, (state, {payload}) => {
+        state.loading = false;
+        state.error = payload;
+      });
+  },
 });
 
 export default authSlice.reducer;
