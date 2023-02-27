eda9dac6e99c1d63abfab2f070292d6af2d07254
Aazad Dahal <aazad.dahal@gurzu.net>
2023-02-27 16:40:02 +0545

    feat: Create external stylesheet for HomeScreen

---
+++ src/screens/main/HomeScreen/HomeScreenStyles.js
@@ -0,0 +1,25 @@
+import {StyleSheet} from 'react-native';
+
+import {Colors} from '@app/constants';
+import {height, heightToDp, width, widthToDp} from '@app/utils';
+
+export const Styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.whiteColor,
+    paddingTop: heightToDp(55),
+  },
+  textStyle: {
+    color: Colors.primaryTextColor,
+    paddingBottom: heightToDp(35),
+    fontFamily: 'Poppins',
+    fontSize: widthToDp(16),
+    fontWeight: '600',
+  },
+  mainItemContainer: {
+    flex: 1,
+    paddingHorizontal: widthToDp(30),
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
