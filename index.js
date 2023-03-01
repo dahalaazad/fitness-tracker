7e65547c6441c1abb5c2696b60e386eccf56c579
Aazad Dahal <aazad.dahal@gurzu.net>
2023-03-01 15:41:57 +0545

    feat: Pass redux-store to ApiInterceptor file

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ import {Provider} from 'react-redux';
 import App from './App';
 import {name as appName} from './app.json';
 import {PersistGate} from 'redux-persist/integration/react';
+import {setupInterceptor} from '@app/utils';
+
+setupInterceptor(store);
 
 const Root = () => {
   return (
