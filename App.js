8040edfc265a0332431ac9c438119b42b6bbadfb
Aazad Dahal <aazad.dahal@gurzu.net>
2023-03-01 16:51:02 +0545

    feat: Refactor for MainStack

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import {useSelector} from 'react-redux';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 
-import {AuthStack, HomeStack, ProfileStack} from '@app/routes';
+import {AuthStack, MainStack} from '@app/routes';
 
 const Stack = createStackNavigator();
 
@@ -12,14 +12,10 @@ const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{headerShown: false}}>
-        {!isLoggedIn ? (
-          <Stack.Screen name="AuthStack" component={AuthStack} />
+        {isLoggedIn ? (
+          <Stack.Screen name="MainStack" component={MainStack} />
         ) : (
-          <Stack.Group>
-            <Stack.Screen name="HomeStack" component={HomeStack} />
-
-            <Stack.Screen name="ProfileStack" component={ProfileStack} />
-          </Stack.Group>
+          <Stack.Screen name="AuthStack" component={AuthStack} />
         )}
       </Stack.Navigator>
     </NavigationContainer>
