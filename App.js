4f9b446066af2ce8279bb6184f46fb4df4b9b93a
Aazad Dahal <aazad.dahal@gurzu.net>
2023-03-01 16:18:00 +0545

    feat: Conditional stack rendering

--- App.js
+++ App.js
@@ -1,18 +1,26 @@
-import {AuthStack, HomeStack, ProfileStack} from '@app/routes';
+import {useSelector} from 'react-redux';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+
+import {AuthStack, HomeStack, ProfileStack} from '@app/routes';
 
 const Stack = createStackNavigator();
 
 const App = () => {
+  const isLoggedIn = useSelector(state => state?.auth?.isLoggedIn);
+
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{headerShown: false}}>
-        <Stack.Screen name="AuthStack" component={AuthStack} />
+        {!isLoggedIn ? (
+          <Stack.Screen name="AuthStack" component={AuthStack} />
+        ) : (
+          <Stack.Group>
+            <Stack.Screen name="HomeStack" component={HomeStack} />
 
-        <Stack.Screen name="HomeStack" component={HomeStack} />
-
-        <Stack.Screen name="ProfileStack" component={ProfileStack} />
+            <Stack.Screen name="ProfileStack" component={ProfileStack} />
+          </Stack.Group>
+        )}
       </Stack.Navigator>
     </NavigationContainer>
   );
