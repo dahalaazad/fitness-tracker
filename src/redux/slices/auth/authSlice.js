f0bd296fce78340179f23d4d2857ceb5030a0e56
Aazad Dahal <aazad.dahal@gurzu.net>
2023-03-02 11:20:27 +0545

    feat: Create function for logout in authSlice

--- src/redux/slices/auth/authSlice.js
+++ src/redux/slices/auth/authSlice.js
@@ -25,6 +25,20 @@ export const authUser = createAsyncThunk(
     }
   },
 );
+
+export const logoutUser = createAsyncThunk(
+  'auth/logoutUser',
+  async (_, {dispatch, rejectWithValue}) => {
+    try {
+      const logoutResponse = await mercuryAPI.delete('logout');
+      dispatch(setLoggedIn(false));
+      return logoutResponse;
+    } catch (error) {
+      return rejectWithValue(error);
+    }
+  },
+);
+
 export const authSlice = createSlice({
   name: 'auth',
   initialState,
@@ -46,11 +60,27 @@ export const authSlice = createSlice({
         state.error = null;
       })
       .addCase(authUser.fulfilled, (state, {payload}) => {
+        console.log(payload);
         state.loading = false;
         state.isLoggedIn = true;
         state.userInfo = payload?.data?.data;
+        state.userToken = payload?.headers?.authorization;
       })
       .addCase(authUser.rejected, (state, {payload}) => {
+        state.loading = false;
+        state.error = payload;
+      })
+      .addCase(logoutUser.pending, state => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(logoutUser.fulfilled, (state, {payload}) => {
+        state.loading = false;
+        state.isLoggedIn = false;
+        state.userInfo = {};
+        state.userToken = null;
+      })
+      .addCase(logoutUser.rejected, (state, {payload}) => {
         state.loading = false;
         state.error = payload;
       });
