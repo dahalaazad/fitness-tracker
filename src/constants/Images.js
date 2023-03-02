ee77101f12f8b86b2b0e8664eb24eca933780f5f
Aazad Dahal <aazad.dahal@gurzu.net>
2023-03-02 12:45:32 +0545

    feat: Add placeholder image for userIcon

--- src/constants/Images.js
+++ src/constants/Images.js
@@ -2,4 +2,5 @@ export const Images = {
   loginMainBg: require('@app/assets/images/login-main-bg.png'),
   loginEmailBg: require('@app/assets/images/login-email-bg.png'),
   userImageIcon: require('@app/assets/images/user-image-icon.png'),
+  profilePlaceholderIcon: require('@app/assets/images/profile-placeholder-icon.jpg'),
 };
