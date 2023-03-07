ab88526abe7f01f3764ba38dc89c9c147e390cc9
Aazad Dahal <aazad.dahal@gurzu.net>
2023-03-07 23:53:19 +0545

    feat: Add svg icons

--- src/assets/svg/index.js
+++ src/assets/svg/index.js
@@ -4,5 +4,20 @@ import AppleLogo from './apple-logo.svg';
 import EmailIcon from './email-icon.svg';
 import PWIcon from './pw-icon.svg';
 import SkippingIcon from './skipping-icon.svg';
+import AddWorkoutIcon from './add-workout-icon.svg';
+import CrownIcon from './crown-icon.svg';
+import LogoutIcon from './logout-icon.svg';
+import WixxIcon from './wixx-icon.svg';
 
-export {GoogleLogo, FbLogo, AppleLogo, EmailIcon, PWIcon, SkippingIcon};
+export {
+  GoogleLogo,
+  FbLogo,
+  AppleLogo,
+  EmailIcon,
+  PWIcon,
+  SkippingIcon,
+  AddWorkoutIcon,
+  CrownIcon,
+  LogoutIcon,
+  WixxIcon,
+};
