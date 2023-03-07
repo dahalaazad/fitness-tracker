b22f1f68aa39c077c27d5ec7a978ede6aab9fcf9
Aazad Dahal <aazad.dahal@gurzu.net>
2023-03-07 14:16:52 +0545

    feat: Remove unused CustomHeader component

--- src/components/atoms/index.js
+++ src/components/atoms/index.js
@@ -3,6 +3,5 @@ export * from './InputField';
 export * from './UserImageIcon';
 export * from './ExerciseCard';
 export * from './WorkoutCard';
-export * from './CustomHeader';
 export * from './HeaderBackIcon';
 export * from './HomeScreenHeaderItems';
