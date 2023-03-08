d5e0a6eebe5f0c2743539fa19951d19450c3b05a
Aazad Dahal <aazad.dahal@gurzu.net>
2023-03-08 18:34:22 +0545

    feat: Complete spinner with overlay

--- src/screens/profile/ProfileScreen/ProfileScreen.js
+++ src/screens/profile/ProfileScreen/ProfileScreen.js
@@ -1,10 +1,14 @@
 import {StyleSheet, Text, View, Button} from 'react-native';
-import {useDispatch} from 'react-redux';
+import {useSelector, useDispatch} from 'react-redux';
+
+import Spinner from 'react-native-loading-spinner-overlay';
 
 import {logoutUser} from '@app/redux/slices/auth/authSlice';
+import {Colors} from '@app/constants';
 
 const ProfileScreen = ({navigation}) => {
   const dispatch = useDispatch();
+  const loading = useSelector(state => state?.auth?.loading);
 
   const logout = () => {
     dispatch(logoutUser())
@@ -20,6 +24,13 @@ const ProfileScreen = ({navigation}) => {
 
   return (
     <View style={styles.container}>
+      <Spinner
+        visible={loading}
+        color={Colors.whiteColor}
+        overlayColor={Colors.loginOverlayColor}
+        animation="fade"
+      />
+
       <Text style={styles.textStyle}>ProfileScreen</Text>
 
       <Button title="Logout" onPress={logout} />
