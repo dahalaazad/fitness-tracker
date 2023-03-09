7f1c4dd99e57dacd67e693a6806461a8945d4a04
Aazad Dahal <aazad.dahal@gurzu.net>
2023-03-09 22:53:40 +0545

    feat: Complete WorkoutCard design

--- src/components/atoms/WorkoutCard/WorkoutCard.js
+++ src/components/atoms/WorkoutCard/WorkoutCard.js
@@ -1,9 +1,34 @@
-import {View, Text} from 'react-native';
+import {View, Text, TouchableOpacity} from 'react-native';
+
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+import {Colors} from '@app/constants';
+import {Styles} from './WorkoutCardStyles';
 
 const WorkoutCard = () => {
   return (
-    <View>
-      <Text>WorkoutCard</Text>
+    <View style={Styles.container}>
+      <TouchableOpacity style={Styles.playIconContainer}>
+        <Ionicons
+          name="play-circle-outline"
+          color={Colors.primaryRedColor}
+          size={34}
+        />
+      </TouchableOpacity>
+
+      <View style={Styles.rightContainer}>
+        <View style={Styles.topTextContainer}>
+          <Text style={Styles.topTextStyle}>Morning workout</Text>
+
+          <View style={Styles.bottomTextContainer}>
+            <Text style={Styles.bottomTextStyle}>5 workout</Text>
+
+            <Text style={Styles.textSeperatorStyle}>.</Text>
+
+            <Text style={Styles.bottomTextStyle}>25:00 </Text>
+          </View>
+        </View>
+      </View>
     </View>
   );
 };
---
+++ src/components/atoms/WorkoutCard/WorkoutCardStyles.js
@@ -0,0 +1,55 @@
+import {StyleSheet} from 'react-native';
+
+import {Colors} from '@app/constants';
+import {height, heightToDp, width, widthToDp} from '@app/utils';
+
+export const Styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    marginHorizontal: widthToDp(30),
+    marginVertical: heightToDp(8),
+    borderRadius: widthToDp(20),
+    elevation: 3,
+    shadowRadius: 20,
+    shadowColor: Colors.boxShadowColor,
+  },
+  playIconContainer: {
+    flex: 1,
+    backgroundColor: Colors.workoutCardRedColor,
+    paddingVertical: heightToDp(30),
+    paddingHorizontal: widthToDp(12),
+    borderTopLeftRadius: widthToDp(20),
+    borderBottomLeftRadius: widthToDp(20),
+  },
+  rightContainer: {
+    flex: 9,
+    backgroundColor: Colors.whiteColor,
+    justifyContent: 'center',
+    paddingLeft: widthToDp(10),
+    borderTopRightRadius: widthToDp(20),
+    borderBottomRightRadius: widthToDp(20),
+  },
+  topTextContainer: {
+    paddingBottom: heightToDp(4),
+  },
+  topTextStyle: {
+    fontFamily: 'Poppins',
+    fontSize: widthToDp(16),
+    fontWeight: '500',
+  },
+  bottomTextContainer: {
+    flexDirection: 'row',
+    paddingTop: heightToDp(4),
+  },
+  bottomTextStyle: {
+    fontFamily: 'Poppins',
+    fontSize: widthToDp(14),
+    fontWeight: '400',
+  },
+  textSeperatorStyle: {
+    fontFamily: 'Poppins',
+    fontWeight: '500',
+    color: Colors.primaryTextColor,
+    paddingHorizontal: widthToDp(4),
+  },
+});
