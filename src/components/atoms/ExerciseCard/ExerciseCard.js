676c9aefa92fda20c1b008589bf1ef1e2b55bcda
Aazad Dahal <aazad.dahal@gurzu.net>
2023-03-11 00:46:02 +0545

    feat: Create component for ExerciseCard

---
+++ src/components/atoms/ExerciseCard/ExerciseCard.js
@@ -0,0 +1,11 @@
+import {View, Text} from 'react-native';
+
+const ExerciseCard = () => {
+  return (
+    <View>
+      <Text>ExerciseCard</Text>
+    </View>
+  );
+};
+
+export default ExerciseCard;
--- src/components/atoms/index.js
+++ src/components/atoms/index.js
@@ -3,4 +3,5 @@ export * from './InputField';
 export * from './UserImageIcon';
 export * from './CardComponent';
 export * from './WorkoutCard';
+export * from './ExerciseCard';
 export * from './HomeScreenHeaderItems';
