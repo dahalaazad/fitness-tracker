75604b1d067919c41e483de310967f6eb3ed7d34
Aazad Dahal <aazad.dahal@gurzu.net>
2023-03-11 21:03:53 +0545

    feat: Error handling with toast message

--- src/utils/ApiInterceptor.js
+++ src/utils/ApiInterceptor.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import Config from 'react-native-config';
+import {showToast} from './ShowToast';
 
 export const mercuryAPI = axios.create({
   baseURL: Config.API_URL,
@@ -23,6 +24,27 @@ export function setupInterceptor(store) {
   mercuryAPI.interceptors.response.use(
     response => response,
     error => {
+      let errorMessage = error?.response?.data?.message;
+
+      switch (error?.response?.status) {
+        case 401:
+          showToast(
+            'error',
+            'Warning',
+            errorMessage === 'Signature has expired'
+              ? 'Session has expired'
+              : errorMessage,
+          );
+          break;
+        case 404:
+          showToast('error', 'Warning', 'Please connect to a network');
+          break;
+        case 422:
+          showToast('error', 'Warning', 'Please check your input format');
+          break;
+        default:
+          showToast('error', 'Warning', 'Oops! Something went wrong.');
+      }
       return Promise.reject(error);
     },
   );
